b78f6c43fe842409efca747fbedecfeacee486af
Rian <rian.azevedo@qintess.com>
2023-01-29 15:12:35 -0300

    Componente sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Header } from '@/components/Header'
+import { Flex } from '@chakra-ui/react'
+import { Sidebar } from '@/components/Sidebar'
 
 
 export default function Dashboard() {
     return (
-        <Header></Header>
-        )
+        <Flex direction="column" h="100vh">
+            <Header />
+            <Flex w="100%" marginY="6" maxWidth={1480} marginX="auto" paddingX="6">
+                <Sidebar />
+            </Flex>
+        </Flex>
+    )
 }
