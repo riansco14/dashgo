1e6bced4e1e9097768d1ea8776749ec39fbfa948
Rian <rian.azevedo@qintess.com>
2023-02-09 11:38:37 -0300

    add menu swipeable

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { useSidebarDrawer } from '@/contexts/SidebarDrawerContext'
 import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
 import React from 'react'
 import { RiMenuLine } from 'react-icons/ri'
+import { useSwipeable } from 'react-swipeable'
 import { Logo } from '../Header/Logo'
 import { NotificationsNav } from '../Header/NotificationsNav'
 import { Profile } from '../Header/Profile'
@@ -12,6 +13,25 @@ export function Header() {
 
     const isLargeVersion = useBreakpointValue({ base: false, lg: true })
 
+    const { ref: documentRef } = useSwipeable({
+        onSwiped: ({ dir, event }) => {
+            if (dir === "Right" && event.changedTouches[0].clientX < 240) {
+                onOpen()
+            }
+            console.log(event.changedTouches[0].clientX)
+        },
+        preventDefaultTouchmoveEvent: true
+    });
+    // attach swipeable to document
+    React.useEffect(() => {
+        if (!isLargeVersion)
+            documentRef(document);
+        else {
+            documentRef({});
+        }
+        // Clean up swipeable event listeners
+        return () => documentRef({});
+    }, [isLargeVersion]);
 
     return (<Flex
         as="header"
